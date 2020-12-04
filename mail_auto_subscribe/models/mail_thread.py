105feb7709afff8f21424e1f9b45519cc72824d7
nmadhoc <nm@adhoc.com.ar>
2020-12-04 10:06:56 -0300

    [FIX] mail_auto_subscribe

--- mail_auto_subscribe/models/mail_thread.py
+++ mail_auto_subscribe/models/mail_thread.py
@@ -8,21 +8,28 @@ from odoo import models
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
-    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
-        # TODO evaluar si existe el mismo modelo en más de una etiqueta
+    def message_subscribe(
+        self, partner_ids=None, channel_ids=None, subtype_ids=None, customer_ids=None
+    ):
         model_tags = self.env["res.partner.category"].search(
             [("auto_subscribe", "=", True), ("model_ids.model", "=", self._name)]
         )
         if model_tags:
-            subscribers = (
-                self.env["res.partner"]
-                .search(
+            added_partners = []
+            for partner_id in partner_ids:
+                subscribers = self.env["res.partner"].search(
                     [
-                        ("parent_id", "=", self.partner_id.id),
+                        (
+                            "commercial_partner_id",
+                            "=",
+                            self.env["res.partner"]
+                            .browse(partner_id)
+                            .commercial_partner_id.id,
+                        ),
+                        ("id", "!=", partner_id),
                         ("category_id", "in", model_tags.ids),
                     ]
                 )
-                .ids
-            )
-            partner_ids.extend(subscribers)
+                added_partners.extend(subscribers.ids)
+            partner_ids.extend(added_partners)
         return super(MailThread, self).message_subscribe(partner_ids)
